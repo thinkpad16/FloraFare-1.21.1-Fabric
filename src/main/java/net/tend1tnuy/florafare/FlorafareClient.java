69840e3e97a90c2b639b197b260dca688eb8149f
thinkpad16 <pasechnik.danil2012@gmail.com>
2026-06-21 17:56:36 +0300

    Initial commit

---
+++ src/main/java/net/tend1tnuy/florafare/FlorafareClient.java
@@ -0,0 +1,10 @@
+package net.tend1tnuy.florafare;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlorafareClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tend1tnuy/florafare/FlorafareDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tend1tnuy.florafare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlorafareDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
